6048a3242f755e8e67fcc74c93d7831fcc4aa8d6
Jia GUO <JGuo15@slb.com>
2020-03-07 20:39:35 +0800

    sender

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Send
 {
-    class Program
+    class Send
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Before sending...");
+
+            var factory = new ConnectionFactory(){
+                HostName = "localhost"
+            };
+
+            using(var connection = factory.CreateConnection()){
+                using(var channel = connection.CreateModel()){
+                    channel.QueueDeclare(
+                        queue: "qhello",
+                        durable:false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments:null
+                    );
+
+                    string message = "hello rabbit-mq";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange:"",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body
+                    );
+
+                    Console.WriteLine($"{message} sent");
+                }
+            }
+
+            Console.WriteLine("After sending...");
+            Console.ReadLine();
         }
     }
 }
